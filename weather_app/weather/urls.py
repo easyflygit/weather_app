2c45273b7952c664939d57f274031348348f7b10
easyflygit <akv888@inbox.ru>
2024-07-18 19:05:13 +0200

    добавил ссылку на более детальную информацию о погоде в previously viewed

--- weather_app/weather/urls.py
+++ weather_app/weather/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/stats/', views.SearchHistoryStats.as_view(), name='search-history-stats'),
+    path('api/weather/', views.WeatherView.as_view(), name='weather'),
     path('city-autocomplete/', views.city_autocomplete, name='city_autocomplete'),
     path('signup/', views.signup, name='signup'),
 ]
